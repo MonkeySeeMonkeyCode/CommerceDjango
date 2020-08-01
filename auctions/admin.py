aba538cfcb1a5fc15be20b5be840cedf3e068e5d
Monkey Do <monkeyscapegoat@gmail.com>
2020-08-01 02:37:39 -0400

    working on models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Listing, Bids, Comments, Category
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(Category)
--- auctions/models.py
+++ auctions/models.py
@@ -5,11 +5,39 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Listing():
-    pass
+class Category(models.Model):
+    category = models.CharField(max_length=64)
 
-class Bids():
-    pass
+    def __str__(self):
+        return f"{self.category}"
 
-class Comments():
-    pass
+class Listing(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=250)
+    bid = models.IntegerField()
+    createdby = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listingcreator")
+    image = models.CharField(max_length=250, blank=True)
+    category = models.ManyToManyField(Category, blank=True, related_name='Listing_category')
+    active = models.BooleanField()
+
+    def __str__(self):
+        return f"{title}"
+
+class Bids(models.Model):
+    bid = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return f"{bid} on {item} by {user.username}"
+
+class Comments(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Comment_creator")
+    datetime = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment on {listing.title} by {user.username}"
